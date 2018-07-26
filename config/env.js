841f6c196237424c2c3c2bc4ff9abad25e648d62
Jakub Dziekan <jdziek89@gmail.com>
2018-07-26 17:50:09 +0100

    again

--- config/env.js
+++ config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 4000,
-  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/test-venue-scanner'
+  db: process.env.MONGODB_URI || 'mongodb://heroku_dhtd83qc:jsn53ikqdkla3ailinuhsjkafu@ds155461.mlab.com:55461/heroku_dhtd83qc'
 
 };
